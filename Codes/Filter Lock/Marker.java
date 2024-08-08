69787eb3d11667a8686bd2189846681b7b7d15e2
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-08 05:04:14 +0200

    Update Marker.java

--- Codes/Filter Lock/Marker.java
+++ Codes/Filter Lock/Marker.java
@@ -1,1 +1,33 @@
 
+import java.util.Queue;
+import java.util.concurrent.locks.Lock;
+
+public class Marker extends Thread {
+    private final Lock lock;
+    private final Output output;
+    private final Queue<Paper> allPapers;
+
+    public Marker(Threeterson lock, Output output, Queue<Paper> allPapers) {
+        this.lock = lock;
+        this.output=output;
+        this.allPapers=allPapers;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            lock.lock();
+            try {
+                if(allPapers.isEmpty()){
+                    break;
+                } 
+                Paper paper = allPapers.poll();
+                if(paper != null) {
+                    paper.mark();
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
