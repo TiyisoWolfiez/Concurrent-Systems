bffb67bf58655794a607a868bf01a68c21f5c13b
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-08-08 05:04:56 +0200

    Update OutputValidator.java

--- Codes/Filter Lock/OutputValidator.java
+++ Codes/Filter Lock/OutputValidator.java
@@ -1,1 +1,13 @@
 
+public class OutputValidator {
+    private final Output output;
+   
+
+    public OutputValidator(Output output) {
+        this.output = output;
+    }
+
+    public boolean validate() {
+        return true;
+    }    
+}
