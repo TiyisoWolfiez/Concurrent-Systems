8106715e4d384b4b8472ab01c0f85ad266347204
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-10-20 11:37:55 +0200

    Update Test.java

--- Codes/Concurrent Testing Framework/Test.java
+++ Codes/Concurrent Testing Framework/Test.java
@@ -1,1 +1,17 @@
+import java.lang.annotation.*;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Test {}
+
+
+/*
+ Fine-Grained Synchronization:
+    This involves locking smaller sections of code or individual objects.
+
+ Optimistic Synchronization:
+    This approach assumes that conflicts are rare and uses techniques like compare-and-swap operations.
+
+ Course-Grained Synchronization:
+    This type of synchronization involves locking larger sections of code or entire methods.
+ */
