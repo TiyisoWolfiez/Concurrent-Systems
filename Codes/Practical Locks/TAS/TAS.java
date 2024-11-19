65c99972d3d6b92c2ea8771f88be95143bcaa942
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-11-19 10:11:05 +0200

    Update TAS.java

--- Codes/Practical Locks/TAS/TAS.java
+++ Codes/Practical Locks/TAS/TAS.java
@@ -1,1 +1,39 @@
+// IMPLEMENTATION OF LOCK USING TEST-AND-SET
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 
+public class TAS implements Lock {
+	AtomicBoolean state = new AtomicBoolean(false);
+
+	@Override
+	public void lock() {
+		while (state.getAndSet(true)) {}
+	}
+
+	@Override
+	public void lockInterruptibly() throws InterruptedException {
+	}
+
+	@Override
+	public Condition newCondition() {
+		return null;
+	}
+
+	@Override
+	public boolean tryLock() {
+		return false;
+	}
+
+	@Override
+	public boolean tryLock(long arg0, TimeUnit arg1)
+			throws InterruptedException {
+		return false;
+	}
+
+	@Override
+	public void unlock() {
+		state.set(false);
+	}
+}
