92a94dbae8b6b11033105dbe64a13b54ad34a6a5
tiyiso. <110611386+TiyisoWolfiez@users.noreply.github.com>
2024-11-19 09:49:32 +0200

    Update BackoffLock.java

--- Codes/Practical Locks/Exponential Backoff/BackoffLock.java
+++ Codes/Practical Locks/Exponential Backoff/BackoffLock.java
@@ -1,1 +1,68 @@
+// IMPLEMENTATION OF BACKOFF LOCK
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+
+public class BackoffLock implements Lock{
+
+	private AtomicBoolean state = new AtomicBoolean(false);
+	private static int MIN_DELAY ;
+	private static int MAX_DELAY ;
+
+	public BackoffLock(int min,int max) {
+		MIN_DELAY = min;
+		MAX_DELAY = max;
+		
+	}
+	
+	@Override
+	public void lock() {
+		Backoff backoff = new Backoff(MIN_DELAY, MAX_DELAY);
+		while (true) 
+		{
+			while (state.get()) {};
+			if (!state.getAndSet(true)) 
+			{
+				return;
+			}
+			else 
+			{
+				try 
+				{
+					backoff.backoff();
+				} catch (InterruptedException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void lockInterruptibly() throws InterruptedException {
+	}
+
+	@Override
+	public Condition newCondition() {
+		return null;
+	}
+
+	@Override
+	public boolean tryLock() {
+		return false;
+	}
+
+	@Override
+	public boolean tryLock(long arg0, TimeUnit arg1)
+			throws InterruptedException {
+		return false;
+	}
+
+	@Override
+	public void unlock() {
+		state.set(false);
+	}
+}
